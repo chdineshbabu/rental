82a85bfb9842367b045aa40fa974614a3ed60a84
Dinesh Ch <70335174+chdineshbabu@users.noreply.github.com>
2023-08-12 19:08:06 +0530

    Finished backend

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,7 +11,7 @@ function Cards(props) {
         
   <div class="nft">
     <div class='main'>
-      <img class='tokenImage' src="https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="NFT" />
+      <img class='tokenImage' src={post.imgurl} alt="NFT" />
       <h2>{post.property}</h2>
       <p class='description'>{post.description}</p>
       <div class='tokenInfo'>
