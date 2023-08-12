6a5cb3f4bedcb9ffbcc9127813df59138975e693
Dinesh Ch <70335174+chdineshbabu@users.noreply.github.com>
2023-08-12 16:51:04 +0530

    Form uploding stage done

--- src/components/DashForm.js
+++ src/components/DashForm.js
@@ -1,36 +1,99 @@
-import React from 'react'
+import { useForm } from "react-hook-form"
+import *as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { addDoc, collection } from 'firebase/firestore'
+import { auth, db } from "../configuration/firebase"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+import { storage } from "../configuration/firebase"
+import { ref, uploadBytes, getStorage } from 'firebase/storage'
+import { v4 } from 'uuid';
+import { getDownloadURL } from "firebase/storage"
 function DashForm() {
+    const navigate = useNavigate()
+
+
+    //Auth Profile 
+    const [user] = useAuthState(auth)
+    //Yup Form Validation
+    const schema = yup.object().shape({
+        property: yup.string().required('You Must Add a property Name'),
+        description: yup.string().required('You Must Add a Descriptiopn'),
+        price: yup.number().required('You Must Add a Price'),
+        location: yup.string().required('You Must Add a Location'),
+
+    });
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
+    })
+
+    //Firebase database Storage Config
+    const postsRef = collection(db, "sells");
+const onCreatePost = async (data) => {
+        await addDoc(postsRef, {
+            ...data,
+            username: user?.displayName,
+            userId: user?.uid,
+        })
+        navigate("/dashboard")
+    }
+    // Image storage config
+    const [imageUpload, setImageUpload] = useState(null);
+
+    const uploadImage = () => {
+        if (imageUpload == null) return;
+        const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+        uploadBytes(imageRef, imageUpload).then(() => {
+            alert("Image Uploded")
+        });
+
+        getDownloadURL(imageRef)
+            .then((url) => {
+                console.log(url)
+            })
+            .catch((error) => {
+                console.error('Error getting download URL:', error);
+            });
+
+    }
+
+
     return (
         <div className='dash-form '>
             <h1 className='text-white text-3xl font-semibold'>Sell Now</h1>
             <div className='form-db'>
-                <form>
+                <form onSubmit={handleSubmit(onCreatePost)} >
                     <div class="group">
-                        <label className='text-white py-2' for="name">Property</label>
-                        <input type="text" id="name" className='bg-transparent text-white' placeholder="" /><hr style={{ width: "300px" }} />
+                        <label className='text-white py-1' for="name">Property</label>
+                        <input type="text" id="name" className='bg-transparent text-white' placeholder=""  {...register('property')} /><hr style={{ width: "300px" }} />
+                        <p>{errors.property?.message}</p>
                     </div>
                     <div class="group">
-                        <label className='text-white py-2' for="name">Description</label>
-                        <input type="textarea" id="name" className='bg-transparent text-white' placeholder="" /><hr style={{ width: "300px" }} />
+                        <label className='text-white py-1' for="name">Description</label>
+                        <input type="textarea" id="name" className='bg-transparent text-white' placeholder=""  {...register('description')} /><hr style={{ width: "300px" }} />
+                        <p>{errors.description?.message}</p>
                     </div>
                     <div class="group">
-                        <label className='text-white py-2' for="name">Price</label>
-                        <input type="text" id="name" className='bg-transparent text-white' placeholder="" /><hr style={{ width: "300px" }} />
+                        <label className='text-white py-1' for="name">Price</label>
+                        <input type="text" id="name" className='bg-transparent text-white' placeholder=""  {...register('price')} /><hr style={{ width: "300px" }} />
+                        <p>{errors.price?.message}</p>
                     </div>
                     <div class="group">
-                        <label className='text-white py-2' for="name">Location</label>
-                        <input type="text" id="name" className='bg-transparent text-white' placeholder="" /><hr style={{ width: "300px" }} />
+                        <label className='text-white py-1' for="name">Location</label>
+                        <input type="text" id="name" className='bg-transparent text-white' placeholder=""  {...register('location')} /><hr style={{ width: "300px" }} />
+                        <p>{errors.location?.message}</p>
                     </div>
                     <label class="block my-10 ">
                         <span class="sr-only">Choose profile photo</span>
-                        <input type="file" class="block w-full text-sm text-slate-500
+                        <input type="file" onChange={(event) => { setImageUpload(event.target.files[0]) }} class="block w-full text-sm text-slate-500
                                                  file:mr-4 file:py-2 file:px-4
                                                 file:rounded-full file:border-0
                                                 file:text-sm file:font-semibold
                                               file:bg-violet-50 file:text-violet-700
                                             hover:file:bg-violet-100"/>
                     </label>
-                    <button className='form-btn'>Add</button>
+                    <input className="form-btn" onClick={uploadImage} type="submit" value="Publish" />
                 </form>
             </div>
         </div>
--- src/configuration/firebase.js
+++ src/configuration/firebase.js
@@ -1,18 +1,18 @@
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 import {getFirestore} from 'firebase/firestore'
+import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBKprXEZxrwBqpVzt4ySwMoai7rF-5e31A",
-  authDomain: "first-fa438.firebaseapp.com",
-  projectId: "first-fa438",
-  storageBucket: "first-fa438.appspot.com",
-  messagingSenderId: "488833416999",
-  appId: "1:488833416999:web:72224daba7da4fc1a3b302",
-  measurementId: "G-95DVR3V7YJ"
+  apiKey: "AIzaSyC_x1cvFpVPG1ust6qyakCg9JyjtwunL_o",
+  authDomain: "rental-d77b0.firebaseapp.com",
+  databaseURL: "https://rental-d77b0-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "rental-d77b0",
+  storageBucket: "rental-d77b0.appspot.com",
+  messagingSenderId: "365507671292",
+  appId: "1:365507671292:web:256fff45d564f8efcc1379"
 };
 
 // Initialize Firebase
@@ -22,3 +22,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+export const storage = getStorage(app)
